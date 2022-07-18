303b9319b5d1ba948c92307113961559b1926c12
amitjait <amit.rimit591@gmail.com>
2022-07-19 00:13:29 +0530

    Done

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+let main = document.getElementById('app');
+let para = document.getElementById('ptag');
+let swap = document.getElementById('swap');
 
+
+// swap.addEventListener('click', () => {
+//     main.classList = 'night';
+//     swap.classList = 'button_night';
+// });
+swap.addEventListener('click', () => {
+    if(app.classList.contains('day') && swap.classList.contains('button_day')) {
+        main.classList = 'night';
+        swap.classList = 'button_night';
+    } else if(app.classList.contains('night') && swap.classList.contains('button_night')) {
+        main.classList = 'day';
+        swap.classList = 'button_day';
+    }
+});
